e9be7b386300eff52fe5a53d9ae2d10d2583fefd
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-05-17 15:09:19 +0800

    migrated chain related from dashboard

---
+++ lib/chain.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+class Chain extends StatefulWidget {
+  const Chain({Key? key}) : super(key: key);
+
+  @override
+  State<Chain> createState() => _ChainState();
+}
+
+class _ChainState extends State<Chain> {
+  int _currentStep = 0;
+  bool isPublic = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => setState(() => isPublic = !isPublic),
+            icon: Icon(isPublic ? Icons.visibility_off : Icons.visibility),
+          ),
+        ],
+      ),
+      body: StreamBuilder<http.Response>(
+        stream: chainStream(),
+        builder: (context, response) {
+          if (response.hasError) {
+            return Center(child: Text('Error: ${response.error}'));
+          }
+          if (response.hasData) {
+            Map<String, dynamic> json = jsonDecode(response.data!.body);
+
+            final List<Step> steps = generateSteps(json);
+            return Stepper(
+              steps: steps,
+              controlsBuilder: (_, __) => const SizedBox(),
+              currentStep: _currentStep,
+              onStepTapped: (index) => setState(() => _currentStep = index),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Stream<http.Response> chainStream() {
+    return Stream.periodic(const Duration(seconds: 5)).asyncMap((_) async =>
+        await http
+            .get(Uri.parse('https://Blockchain.felixwong6.repl.co/chain')));
+  }
+
+  List<Step> generateSteps(Map<String, dynamic> json) {
+    final List<Step> steps = [];
+
+    final chain = json['chain'] as List<dynamic>;
+    for (Map b in chain) {
+      final tx = b['transaction'];
+      final from = tx['from'];
+      final to = tx['to'];
+      final amount = tx['amount'];
+
+      final nonce = b['nonce'];
+      final prevHash = b['prev_hash'];
+      final hash = b['hash'];
+      final timestamp = b['timestamp'];
+
+      final date =
+          DateTime.fromMillisecondsSinceEpoch(timestamp.toInt() * 1000);
+      final formattedDate = DateFormat("yyyy-MM-dd, HH:mm:ss").format(date);
+
+      final step = Step(
+        isActive: _currentStep == steps.length,
+        title: Text('Block #${steps.length}'),
+        subtitle: Text(formattedDate),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (isPublic) Text('From: $from'),
+            if (isPublic) Text('To: $to'),
+            if (isPublic) Text('Amount: \$$amount'),
+            Text('Nonce: $nonce'),
+            Text('Prev Hash: $prevHash'),
+            Text('Hash: $hash'),
+          ],
+        ),
+      );
+      steps.add(step);
+    }
+    return steps;
+  }
+}
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,83 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-class DashBoard extends StatefulWidget {
+class DashBoard extends StatelessWidget {
   const DashBoard({Key? key}) : super(key: key);
 
   @override
-  State<DashBoard> createState() => _DashBoardState();
-}
-
-class _DashBoardState extends State<DashBoard> {
-  int _currentStep = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<http.Response>(
-        stream: chainStream(),
-        builder: (context, response) {
-          if (response.hasError) {
-            return Center(child: Text('Error: ${response.error}'));
-          }
-          if (response.hasData) {
-            Map<String, dynamic> json = jsonDecode(response.data!.body);
-
-            final List<Step> steps = generateSteps(json);
-            return Stepper(
-              steps: steps,
-              controlsBuilder: (_, __) => const SizedBox(),
-              currentStep: _currentStep,
-              onStepTapped: (index) => setState(() => _currentStep = index),
-            );
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
-      ),
-    );
-  }
-
-  Stream<http.Response> chainStream() {
-    return Stream.periodic(const Duration(seconds: 5)).asyncMap((_) async =>
-        await http
-            .get(Uri.parse('https://Blockchain.felixwong6.repl.co/chain')));
-  }
-
-  List<Step> generateSteps(Map<String, dynamic> json) {
-    final List<Step> steps = [];
-
-    final chain = json['chain'] as List<dynamic>;
-    for (Map b in chain) {
-      final tx = b['transaction'];
-      final from = tx['from'];
-      final to = tx['to'];
-      final amount = tx['amount'];
-
-      final nonce = b['nonce'];
-      final prevHash = b['prev_hash'];
-      final hash = b['hash'];
-      final timestamp = b['timestamp'];
-
-      final step = Step(
-        isActive: _currentStep == steps.length,
-        title: Text('Block #${steps.length}'),
-        content: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('From: $from'),
-            Text('To: $to'),
-            Text('Amount: $amount'),
-            Text('Nonce: $nonce'),
-            Text('Prev Hash: $prevHash'),
-            Text('Hash: $hash'),
-            Text('Timestamp: $timestamp'),
-          ],
-        ),
-      );
-      steps.add(step);
-    }
-    return steps;
+    return Scaffold();
   }
 }
