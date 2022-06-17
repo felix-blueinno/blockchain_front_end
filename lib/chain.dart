b97c58d0f08d69acecd6d789e7994a99053b5789
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-06-17 16:42:44 +0800

    replaced periodic stream with periodic timer

--- lib/chain.dart
+++ lib/chain.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/material.dart';
+import 'package:blockchain_front_end/shared_variables.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class Chain extends StatefulWidget {
@@ -11,101 +13,117 @@ class Chain extends StatefulWidget {
 }
 
 class _ChainState extends State<Chain> {
-  List<Step> steps = [];
+  late Timer timer;
+
   int _currentStep = 0;
   int prevChainLength = 0;
   Key stepperKey = UniqueKey();
   bool isPublic = false;
 
   @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      http
+          .get(Uri.parse('https://Blockchain.felixwong6.repl.co/chain'))
+          .then((response) {
+        if (response.statusCode == 200) {
+          SharedVars.chain = response.body;
+          if (mounted) setState(() {});
+        }
+      }).onError((error, stackTrace) {
+        print(error);
+      });
+
+      http
+          .get(
+              Uri.parse('https://Blockchain.felixwong6.repl.co/unmined_blocks'))
+          .then((response) {
+        if (response.statusCode == 200) {
+          SharedVars.unminedChain = response.body;
+
+          if (mounted) setState(() {});
+        }
+      }).onError((error, stackTrace) {
+        print(error);
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () => setState(() => isPublic = !isPublic),
-            icon: Icon(isPublic ? Icons.visibility_off : Icons.visibility),
-          ),
-        ],
-      ),
-      body: StreamBuilder<http.Response>(
-        stream: chainStream(),
-        builder: (context, response) {
-          if (response.hasData && response.data!.statusCode == 200) {
-            try {
-              Map<String, dynamic> json = jsonDecode(response.data!.body);
-              steps = generateSteps(json);
-
-              if (prevChainLength != steps.length) {
-                prevChainLength = steps.length;
-                stepperKey = UniqueKey();
-              }
-            } catch (e) {
-              print(e);
-            }
-          }
-
-          return steps.isEmpty
-              ? const Center(child: CircularProgressIndicator())
-              : Stepper(
-                  /// Use the same key to prevent stepper repaint every frame
-                  /// this solves issue of stepper crashing when steps amount changes
-                  /// https://github.com/flutter/flutter/issues/27187
-                  key: stepperKey,
-                  steps: steps,
-                  controlsBuilder: (_, __) => const SizedBox(),
-                  currentStep: _currentStep,
-                  onStepTapped: (index) => setState(() => _currentStep = index),
-                );
-        },
-      ),
+      appBar: AppBar(title: const Text('Chain'), actions: [
+        IconButton(
+          onPressed: () => setState(() => isPublic = !isPublic),
+          icon: Icon(isPublic ? Icons.visibility_off : Icons.visibility),
+        ),
+      ]),
+      body: SharedVars.chain.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : buildSteps(),
     );
   }
 
-  Stream<http.Response> chainStream() {
-    return Stream.periodic(const Duration(seconds: 5))
-        .asyncMap((_) async => await http
-            .get(Uri.parse('https://Blockchain.felixwong6.repl.co/chain')))
-        .timeout(const Duration(seconds: 5));
-  }
-
-  List<Step> generateSteps(Map<String, dynamic> json) {
-    final List<Step> steps = [];
-
+  Widget buildSteps() {
+    Map<String, dynamic> json = jsonDecode(SharedVars.chain);
     final chain = json['chain'] as List<dynamic>;
-    for (Map b in chain) {
-      final tx = b['transaction'];
-      final from = tx['from'];
-      final to = tx['to'];
-      final amount = tx['amount'];
 
-      final nonce = b['nonce'];
-      final prevHash = b['prev_hash'];
-      final hash = b['hash'];
-      final timestamp = b['timestamp'];
-
-      final date =
-          DateTime.fromMillisecondsSinceEpoch(timestamp.toInt() * 1000);
-      final formattedDate = DateFormat("yyyy-MM-dd, HH:mm:ss").format(date);
-
-      final step = Step(
-        isActive: _currentStep == steps.length,
-        title: Text('Block #${steps.length + 1}'),
-        subtitle: Text(formattedDate),
-        content: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (isPublic) Text('From: $from'),
-            if (isPublic) Text('To: $to'),
-            if (isPublic) Text('Amount: \$$amount'),
-            Text('Nonce: $nonce'),
-            Text('Prev Hash: $prevHash'),
-            Text('Hash: $hash'),
-          ],
-        ),
-      );
-      steps.add(step);
+    if (prevChainLength != chain.length) {
+      prevChainLength = chain.length;
+      stepperKey = UniqueKey();
     }
-    return steps;
+
+    return Stepper(
+      key: stepperKey,
+      currentStep: _currentStep,
+      onStepTapped: (index) => setState(() => _currentStep = index),
+      controlsBuilder: (_, __) => const SizedBox(),
+      steps: List.generate(
+        chain.length,
+        (index) {
+          final b = chain[index];
+
+          final tx = b['transaction'];
+          final from = tx['from'];
+          final to = tx['to'];
+          final amount = tx['amount'];
+
+          final nonce = b['nonce'];
+          final prevHash = b['prev_hash'];
+          final hash = b['hash'];
+          final timestamp = b['timestamp'];
+
+          final date =
+              DateTime.fromMillisecondsSinceEpoch(timestamp.toInt() * 1000);
+          final formattedDate = DateFormat("yyyy-MM-dd, HH:mm:ss").format(date);
+
+          return Step(
+            state: StepState.complete,
+            isActive: index == _currentStep,
+            title: Text('Block #$index'),
+            subtitle: Text(formattedDate),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (isPublic) Text('From: $from'),
+                if (isPublic) Text('To: $to'),
+                if (isPublic) Text('Amount: \$$amount'),
+                Text('Nonce: $nonce'),
+                Text('Prev Hash: $prevHash'),
+                Text('Hash: $hash'),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
