d6185a231cb7ff914a9647feaee34aa18cac9565
felix-blueinno <94157332+felix-blueinno@users.noreply.github.com>
2022-06-17 16:38:34 +0800

    removed IndexedStack due to performance issue

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:blockchain_front_end/chain.dart';
 import 'package:blockchain_front_end/history.dart';
 import 'package:blockchain_front_end/wallet.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class DashBoard extends StatefulWidget {
   const DashBoard({Key? key}) : super(key: key);
@@ -17,24 +18,22 @@ class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: pageIndex,
-        children: pages,
-      ),
+      body: pages[pageIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: pageIndex,
         showUnselectedLabels: false,
         onTap: (index) => setState(() => pageIndex = index),
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_balance_wallet),
-            label: 'Wallet',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.api_sharp),
-            label: 'Chain',
-          ),
+              icon: Icon(Icons.account_balance_wallet), label: 'Wallet'),
+          BottomNavigationBarItem(icon: Icon(Icons.api_sharp), label: 'Chain'),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            http.get(Uri.parse('https://Blockchain.felixwong6.repl.co/mine')),
+        child: const Icon(Icons.wb_twilight_rounded),
       ),
     );
   }
